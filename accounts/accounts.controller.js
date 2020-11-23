c2270f2fbc2c1054112459b7a8dd86defbf6e476
bentrinh <dungtm112@outlook.com>
2020-11-23 23:34:50 +0700

    [NOTIFICATION] - Handle notification

--- accounts/accounts.controller.js
+++ accounts/accounts.controller.js
@@ -1,101 +1,194 @@
-﻿const express = require('express');
-const router = express.Router();
-const Joi = require('joi');
-const validateRequest = require('_middleware/validate-request');
+﻿const express = require('express')
+const router = express.Router()
+const Joi = require('joi')
+const validateRequest = require('_middleware/validate-request')
 const authorize = require('_middleware/authorize')
-const Role = require('_helpers/role');
-const accountService = require('./account.service');
-const db = require('_helpers/db');
+const Role = require('_helpers/role')
+const accountService = require('./account.service')
+const db = require('_helpers/db')
 
 // routes
-router.post('/authenticate', authenticateSchema, authenticate);
-router.post('/refresh-token', refreshToken);
-router.post('/revoke-token', authorize(), revokeTokenSchema, revokeToken);
-router.post('/register', registerSchema, register);
-router.post('/verify-email', verifyEmailSchema, verifyEmail);
-router.post('/forgot-password', forgotPasswordSchema, forgotPassword);
-router.post('/validate-reset-token', validateResetTokenSchema, validateResetToken);
-router.post('/reset-password', resetPasswordSchema, resetPassword);
-router.get('/', authorize(Role.Admin), getAll);
-router.get('/:id', authorize(), getById);
-router.post('/', authorize(Role.Admin), createSchema, create);
-router.put('/:id', authorize(), updateSchema, update);
-router.delete('/:id', authorize(), _delete);
-router.post('/authorization/:token', authorizationSchema, authorization);
+router.post('/authenticate', authenticateSchema, authenticate)
+router.post('/refresh-token', refreshToken)
+router.post('/revoke-token', authorize(), revokeTokenSchema, revokeToken)
+router.post('/register', registerSchema, register)
+router.post('/verify-email', verifyEmailSchema, verifyEmail)
+router.post('/forgot-password', forgotPasswordSchema, forgotPassword)
+router.post('/validate-reset-token', validateResetTokenSchema, validateResetToken)
+router.post('/reset-password', resetPasswordSchema, resetPassword)
+router.get('/', authorize(Role.Admin), getAll)
+router.get('/:id', authorize(), getById)
+router.post('/', authorize(Role.Admin), createSchema, create)
+router.put('/:id', authorize(), updateSchema, update)
+router.delete('/:id', authorize(), _delete)
+router.post('/authorization/:token', authorizationSchema, authorization)
 router.post("/device-token", authorize(Role.User), submitDeviceToken)
 router.post("/test-fcm", authorize(), testFcm)
 
 
 router.post("/notification", async (req, res, next) => {
-    const { query: { username, sql } } = req,
-        user = await db.Account.findOne({ username })
-    if (!user) res.status(302).json({ message: "Tài khoản chưa đăng nhập mobile" })
+    // const { query: { username, sql } } = req,
+    //     user = await db.Account.findOne({ username })
+    // if (!user) res.status(302).json({ message: "Tài khoản chưa đăng nhập mobile" })
+    // newNoti = new db.Notification({
+    //     user: user._id,
+    //     sql
+    // })
+    // // INSERT INTO Wo_Notifications (`recipient_id`, `notifier_id`,       `post_id`, `comment_id`, `reply_id`, `type`, `type2`, `text`, `url`, `time` ) VALUES (1,27,      35,'0','0','reaction','6','post','index.php?link1=post&id=35',1606139066 )
+    // await newNoti.save()
+    // ["invited_page", "reaction", "comment", "fund_donate", "accepted_request", "accept_group_chat_request", "liked_page"]
+    const { query } = req,
+        username = req.query.username,
+        firstName = req.query.firstName,
+        lastName = req.query.lastName,
+        user = await db.Account.findOne({ username }),
+        index = Object.keys(query).findIndex(item => item === "sql")
+    if (!user) return res.status(302).json({ message: "Tài khoản chưa đăng nhập trên di động!" })
+    let sql = ""
+    Object.keys(query).slice(index, Object.keys(query).length).map(key => {
+        if (key === "sql") {
+            sql += query[key]
+        } else {
+            sql += `&${key}=${query[key]}`
+        }
+    })
+    const lstField = sql.substring(sql.indexOf("(") + 1, sql.indexOf(")")).replaceAll("`", "").replace(/\s/g, '').split(","),
+        subsql = sql.substring(sql.indexOf(")") + 1, sql.length),
+        lstValue = subsql.substring(subsql.indexOf("(") + 1, subsql.indexOf(")")).replaceAll("'", "").replace(/\s/g, '').split(","),
+        type = lstValue[lstField.findIndex(item => item === "type")],
+        post = lstValue[lstField.findIndex(item => item === "post_id")],
+        comment = lstValue[lstField.findIndex(item => item === "comment_id")],
+        reply = lstValue[lstField.findIndex(item => item === "reply_id")],
+        url = lstValue[lstField.findIndex(item => item === "url")]
+    let title = "",
+        body = ""
+    const name = (Boolean(firstName) && Boolean(lastName)) ? `${firstName} ${lastName}` : username
+    switch (type) {
+        case "reaction":
+            title = `${name} đã bày tỏ cảm xúc với bài viết của bạn`
+            body = `Bạn hãy kiểm tra bài viết của bạn nha.`
+            break
+        case "invited_page":
+            title = `${name} đã mời bạn thích trang.`
+            body = `Bạn hãy nhanh tay đồng ý thích trang.`
+            break
+        case "comment":
+            title = `${name} đã bình luận bài viết của bạn.`
+            body = `Bạn hãy nhanh chóng phản hồi bình luận.`
+            break
+        case "fund_donate":
+            title = `${name} đã ủng hộ quỹ.`
+            body = `Bạn hãy nhanh kiểm tra tài khoản để xem hoàn thành kế hoạch chưa.`
+            break
+        case "accepted_request":
+            title = `${name} đã đồng ý lời mời kết bạn của bạn.`
+            body = `Bạn hãy nhanh chóng nhắn tin để làm quen.`
+            break
+        case "accept_group_chat_request":
+            title = `${name} đã đồng ý thanh gia nhóm chat.`
+            body = `Hãy nhanh tay tương tác với ${name} trong nhóm chat nha.`
+            break
+        case "liked_page":
+            title = `${name} đã thích trang.`
+            body = `Hãy đăng thêm thông tin hữu ích để nhiều người cùng like trang.`
+            break
+        default:
+            title = `Bạn có một thông báo mới.`
+            body = `Mở app để kiểm tra. Đừng bỏ lỡ thông báo này.`
+            break
+    }
     newNoti = new db.Notification({
         user: user._id,
+        title,
+        body,
+        url,
         sql
     })
-    // INSERT INTO Wo_Notifications (`recipient_id`, `notifier_id`,       `post_id`, `comment_id`, `reply_id`, `type`, `type2`, `text`, `url`, `time` ) VALUES (1,27,      35,'0','0','reaction','6','post','index.php?link1=post&id=35',1606139066 )
     await newNoti.save()
-    res.status(200).json("Success!")
+    if (user.deviceToken) {
+        const bodyFCM = {
+            to: user.deviceToken,
+            notification: {
+                title: title,
+                body: body,
+                sound: "default",
+                click_action: "FLUTTER_NOTIFICATION_CLICK"
+            },
+            data: {
+                url: url
+            },
+            android: {
+                notification: {
+                    channel_id: "channel_android_default"
+                }
+            }
+        }
+        firebaseCloudMessage(bodyFCM, (err, data) => {
+            if (err) return res.status(302).json({ message: "Có lỗi gì đó ở FCM" })
+            res.status(200).json({ data, message: "Success!" })
+        })
+    } else {
+        res.status(302).json({ message: `${name} chưa cấp quyền thông báo!` })
+    }
 })
 
-module.exports = router;
+module.exports = router
 
 function authenticateSchema(req, res, next) {
     const schema = Joi.object({
         username: Joi.string().required(),
         password: Joi.string().required()
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function authenticate(req, res, next) {
-    const { username, password } = req.body;
-    const ipAddress = req.ip;
+    const { username, password } = req.body
+    const ipAddress = req.ip
     accountService.authenticate({ username, password, ipAddress })
         .then(({ refreshToken, ...account }) => {
-            setTokenCookie(res, refreshToken);
-            res.json(account);
+            setTokenCookie(res, refreshToken)
+            res.json(account)
         })
         .catch(err => {
             console.log(err)
             res.status(302).json({ message: "Tên đăng nhập hoặc mật khẩu không chính xác!" })
-        });
+        })
 }
 
 function refreshToken(req, res, next) {
-    const token = req.cookies.refreshToken;
-    const ipAddress = req.ip;
+    const token = req.cookies.refreshToken
+    const ipAddress = req.ip
     accountService.refreshToken({ token, ipAddress })
         .then(({ refreshToken, ...account }) => {
-            setTokenCookie(res, refreshToken);
-            res.json(account);
+            setTokenCookie(res, refreshToken)
+            res.json(account)
         })
-        .catch(next);
+        .catch(next)
 }
 
 function revokeTokenSchema(req, res, next) {
     const schema = Joi.object({
         token: Joi.string().empty('')
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function revokeToken(req, res, next) {
     // accept token from request body or cookie
-    const token = req.body.token || req.cookies.refreshToken;
-    const ipAddress = req.ip;
+    const token = req.body.token || req.cookies.refreshToken
+    const ipAddress = req.ip
 
-    if (!token) return res.status(400).json({ message: 'Bắt buộc phải có code nha bạn.' });
+    if (!token) return res.status(400).json({ message: 'Bắt buộc phải có code nha bạn.' })
 
     // users can revoke their own tokens and admins can revoke any tokens
     if (!req.user.ownsToken(token) && req.user.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'Unauthorized' })
     }
 
     accountService.revokeToken({ token, ipAddress })
         .then(() => res.json({ message: 'Code đã được thu hồi.' }))
-        .catch(next);
+        .catch(next)
 }
 
 function registerSchema(req, res, next) {
@@ -110,8 +203,8 @@ function registerSchema(req, res, next) {
         acceptTerms: Joi.boolean().valid(true).required(),
         phoneNumber: Joi.string().optional(),
         gender: Joi.string().optional()
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function register(req, res, next) {
@@ -123,46 +216,46 @@ function register(req, res, next) {
             } else {
                 res.status(302).json({ message: "Có lỗi gì đó rồi. Bạn vui lòng kiểm tra lại giúp chúng tớ nha!" })
             }
-        });
+        })
 }
 
 function verifyEmailSchema(req, res, next) {
     const schema = Joi.object({
         token: Joi.string().required()
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function verifyEmail(req, res, next) {
     accountService.verifyEmail(req.body)
         .then(() => res.json({ message: 'Xác thực thành công. Bây giờ bạn có thể đăng nhập rồi.' }))
-        .catch(next);
+        .catch(next)
 }
 
 function forgotPasswordSchema(req, res, next) {
     const schema = Joi.object({
         email: Joi.string().email().required()
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function forgotPassword(req, res, next) {
     accountService.forgotPassword(req.body, req.get('origin'))
         .then(() => res.json({ message: 'Bạn vui lòng kiểm tra email nha. Code đã được gửi tới email của bạn.' }))
-        .catch(next);
+        .catch(next)
 }
 
 function validateResetTokenSchema(req, res, next) {
     const schema = Joi.object({
         token: Joi.string().required()
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function validateResetToken(req, res, next) {
     accountService.validateResetToken(req.body)
         .then(() => res.json({ message: 'Code đã hết hạn' }))
-        .catch(next);
+        .catch(next)
 }
 
 function resetPasswordSchema(req, res, next) {
@@ -170,31 +263,31 @@ function resetPasswordSchema(req, res, next) {
         token: Joi.string().required(),
         password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref('password')).required()
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function resetPassword(req, res, next) {
     accountService.resetPassword(req.body)
         .then(() => res.json({ message: 'Mật khẩu đã được thay đổi. Bây giờ bạn có thể đăng nhập rồi.' }))
-        .catch(next);
+        .catch(next)
 }
 
 function getAll(req, res, next) {
     accountService.getAll()
         .then(accounts => res.json(accounts))
-        .catch(next);
+        .catch(next)
 }
 
 function getById(req, res, next) {
     // users can get their own account and admins can get any account
     if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Chưa được xác thực.' });
+        return res.status(401).json({ message: 'Chưa được xác thực.' })
     }
 
     accountService.getById(req.params.id)
         .then(account => account ? res.json(account) : res.sendStatus(404))
-        .catch(next);
+        .catch(next)
 }
 
 function createSchema(req, res, next) {
@@ -206,14 +299,14 @@ function createSchema(req, res, next) {
         password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
         role: Joi.string().valid(Role.Admin, Role.User).required()
-    });
-    validateRequest(req, next, schema);
+    })
+    validateRequest(req, next, schema)
 }
 
 function create(req, res, next) {
     accountService.create(req.body)
         .then(account => res.json(account))
-        .catch(next);
+        .catch(next)
 }
 
 function updateSchema(req, res, next) {
@@ -224,37 +317,37 @@ function updateSchema(req, res, next) {
         email: Joi.string().email().empty(''),
         password: Joi.string().min(6).empty(''),
         confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
-    };
+    }
 
     // only admins can update role
     if (req.user.role === Role.Admin) {
-        schemaRules.role = Joi.string().valid(Role.Admin, Role.User).empty('');
+        schemaRules.role = Joi.string().valid(Role.Admin, Role.User).empty('')
     }
 
-    const schema = Joi.object(schemaRules).with('password', 'confirmPassword');
-    validateRequest(req, next, schema);
+    const schema = Joi.object(schemaRules).with('password', 'confirmPassword')
+    validateRequest(req, next, schema)
 }
 
 function update(req, res, next) {
     // users can update their own account and admins can update any account
     if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'Unauthorized' })
     }
 
     accountService.update(req.params.id, req.body)
         .then(account => res.json(account))
-        .catch(next);
+        .catch(next)
 }
 
 function _delete(req, res, next) {
     // users can delete their own account and admins can delete any account
     if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'Unauthorized' })
     }
 
     accountService.delete(req.params.id)
         .then(() => res.json({ message: 'Account deleted successfully' }))
-        .catch(next);
+        .catch(next)
 }
 
 // helper functions
@@ -264,13 +357,13 @@ function setTokenCookie(res, token) {
     const cookieOptions = {
         httpOnly: true,
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-    };
-    res.cookie('refreshToken', token, cookieOptions);
+    }
+    res.cookie('refreshToken', token, cookieOptions)
 }
 
 function authorizationSchema(req, res, next) {
-    const schema = Joi.object({});
-    validateRequest(req, next, schema);
+    const schema = Joi.object({})
+    validateRequest(req, next, schema)
 }
 
 function notificationSchema(req, res, next) {
